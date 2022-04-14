ab589237512497695dc3feb450e720b47cb62a46
namoo9009 <namoo9009@naver.com>
2022-04-15 02:26:18 +0900

    메인페이지 완성

--- js/main.js
+++ js/main.js
@@ -3,5 +3,14 @@ window.onload = () => {
 
     const splashOff = setTimeout(() => {
         splash.classList.add('off');
-    },4000)
+    },2100)
+
+    const list = document.querySelectorAll('.navigation .list');
+    function activeNav(){
+        list.forEach((item) => 
+        item.classList.remove('active'));
+        this.classList.add('active');
+    }
+    list.forEach((item) =>
+    item.addEventListener('click',activeNav));
 }
